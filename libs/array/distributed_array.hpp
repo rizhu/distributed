d1bced370d7c69bc6f8d60c3c350a560980dbb42
Richard Hu <r.hu@berkeley.edu>
2022-09-27 17:56:54 -0700

    Fix issue with constructors

--- libs/array/distributed_array.hpp
+++ libs/array/distributed_array.hpp
@@ -1,8 +1,9 @@
+#include <chrono>
 #include <deque>
+#include <thread> 
 #include <variant>
 
 
-#include <omp.h>
 #include "mpi.h"
 
 
@@ -10,57 +11,59 @@ const int SEND_TAG = 0;
 const int RECV_TAG = 1;
 
 
+using namespace std;
+
 template <typename T> class DistributedArray {
+
 public:
-    DistributedArray<T>(int capacity) {
-        MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
-        DistributedArray<T>(capacity, mpi_size);
-    }
+    DistributedArray<T>(int size, int num_processes=0) {
+        MPI_Comm_size(MPI_COMM_WORLD, &this->mpi_size);
+        MPI_Comm_rank(MPI_COMM_WORLD, &this->mpi_rank);
 
-    DistributedArray<T>(int capacity, int num_processes) {
-        MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
-        MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+        this->size = size;
+        if (num_processes > 0)
+            this->num_processes = num_processes;
+        else
+            this->num_processes = this->mpi_size;
 
-        this->capacity = capacity;
-        this->num_processes = num_processes;
-
-        this->local_size = capacity / num_processes;
-        if (this->mpi_rank < capacity % num_processes)
+        this->local_size = this->size / this->num_processes;
+        if (this->mpi_rank < this->size % this->num_processes)
             this->local_size++;
 
-        local_arr = new T[this->local_size];
+        this->local_arr = new T[this->local_size];
     }
+
 
     T get(int index) {
         int owner_rank = index % this->num_processes;
         if (owner_rank == this->mpi_rank) {
-            return this->local_arr[index / num_processes];
+            return this->local_arr[index / this->num_processes];
         }
-        MPI_Request request;
-
     }
+
 
     std::variant<T, MPI_Request> iget(int index) {
 
     }
 
-    void set(int index, T object) {
 
+    void set(int index, T object) {
+        int owner_rank = index % this->num_processes;
+        if (owner_rank == this->mpi_rank) {
+            this->local_arr[index / this->num_processes] = object;
+        }
     }
+
 
     MPI_Request iset(int index, T object) {
 
     }
 
+
 private:
     int mpi_rank, mpi_size;
-    int capacity, num_processes;
+    int size, num_processes;
 
     int local_size;
     T *local_arr;
-protected:
 };
-
-int main(int argc, char* argv[]) {
-    return 0;
-}
